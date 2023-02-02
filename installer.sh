f01ab62b6b835c8bf800b8f8249253c4c201194a
Martin Burchell <mb2353@cam.ac.uk>
2023-02-02 11:41:03 +0000

    Fix paths following flattening

--- installer.sh
+++ installer.sh
@@ -6,7 +6,6 @@
 set -eux -o pipefail
 
 INSTALLER_HOME="$( cd "$( dirname "$0" )" && pwd )"
-PROJECT_ROOT=${INSTALLER_HOME}/..
 
-cd ${PROJECT_ROOT}/docker/dockerfiles
+cd ${INSTALLER_HOME}
 docker compose run --rm crate_workers /bin/bash -c "ls /crate/venv"
