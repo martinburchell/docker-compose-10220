63636e62f9347c3193c8099117593b267efe3b6e
Martin Burchell <mb2353@cam.ac.uk>
2023-02-02 10:43:00 +0000

    Fix ls commands so it breaks again

--- installer/installer.sh
+++ installer/installer.sh
@@ -9,4 +9,4 @@ INSTALLER_HOME="$( cd "$( dirname "$0" )" && pwd )"
 PROJECT_ROOT=${INSTALLER_HOME}/..
 
 cd ${PROJECT_ROOT}/docker/dockerfiles
-docker compose run --rm crate_workers /bin/bash -c ls /crate/venv
+docker compose run --rm crate_workers /bin/bash -c "ls /crate/venv"
