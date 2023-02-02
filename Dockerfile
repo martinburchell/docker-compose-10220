7cb7fcfad67c73c71beb248518aaf586a9373f2b
Martin Burchell <mb2353@cam.ac.uk>
2023-02-02 11:48:56 +0000

    Remove references to CRATE

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,3 @@
 FROM python:3.8-slim-buster
 
-ADD . /crate/src
-
-WORKDIR /crate
-
-
-RUN mkdir -p /crate/venv/foo
+RUN mkdir -p /test/foo
--- installer.sh
+++ installer.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# installer/installer.sh
-
-
 set -eux -o pipefail
 
 INSTALLER_HOME="$( cd "$( dirname "$0" )" && pwd )"
 
 cd ${INSTALLER_HOME}
-docker compose run --rm crate_workers /bin/bash -c "ls /crate/venv"
+docker compose run --rm test /bin/bash -c "ls /test/foo"
